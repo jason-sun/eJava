0c196c8fb4506bea5c4ed5735f2d288269571060
zhenqiang.sun <zhenqiang.sun@erlitech.com>
2017-10-24 09:45:13 +0800

    EHttpClientUtil通过eJava方式直接获取http请求结果

---
+++ src/main/java/com/erlitech/ejava/utils/EHttpClientUtil.java
@@ -0,0 +1,31 @@
+package com.erlitech.ejava.utils;
+
+import com.alibaba.fastjson.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author sunzhenqiang
+ */
+public class EHttpClientUtil {
+
+    // 通过eJava方式直接获取http请求结果，输入输出均为JSONObject
+    public static JSONObject getData(String url, JSONObject joInData) {
+        JSONObject joOutData = new JSONObject();
+        Map<String, Object> params = new HashMap<>();
+        params.put("inData", XEncoder.encode(joInData.toString()));
+
+        try {
+            String result = null;
+            result = XHttpClientUtil.httpPostRequest(url, params);
+            result = XEncoder.decode(result);
+            joOutData = JSONObject.parseObject(result);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        return joOutData;
+    }
+}
